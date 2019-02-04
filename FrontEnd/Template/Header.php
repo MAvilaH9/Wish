028c0b07855d6a77fd8629dd0b53221289cbe9d0
Mario Avila Hu <mavilah9@gmail.com>
2019-02-04 11:50:32 -0600

    Se agrego modal para login

--- FrontEnd/Template/Header.php
+++ FrontEnd/Template/Header.php
@@ -89,7 +89,7 @@
 							</form>
 						</div>
 						
-						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-cart">
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
 							<a href="#"><img src="images/usuario.png" alt=""></a>
 						</div>
 
@@ -102,6 +102,21 @@
 			</div>	
 		</div>
 
+		<!-- Modal Login -->
+		<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+			<div class="container-search-header">
+				<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+					<img src="images/icons/icon-close2.png" alt="CLOSE">
+				</button>
+
+				<form class="wrap-search-header flex-w p-l-15">
+					<button class="flex-c-m trans-04">
+						<i class="zmdi zmdi-search"></i>
+					</button>
+					<input class="plh3" type="text" name="search" placeholder="Search...">
+				</form>
+			</div>
+		</div>
 		<!-- Header Mobile -->
 		<div class="wrap-header-mobile">
 			<!-- Logo moblie -->		
